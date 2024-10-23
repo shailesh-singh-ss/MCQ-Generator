c308eda1a6c5373ceac7ce1a1443f47f4d74a01f
Shailesh Singh <shaileshsingh121122@gmail.com>
2024-10-23 14:48:07 +0530

    Update .gitignore and fix PDF reading error in utils.py

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -7,14 +7,14 @@ import traceback
 def read_file(file):
     if file.name.endswith(".pdf"):
         try:
-            pdf_reader=PyPDF2.PdfFileReader(file)
+            pdf_reader=PyPDF2.PdfReader(file)
             text =""
             for page in pdf_reader.pages:
                 text+=page.extract_text()
             return text
         
         except Exception as e:
-            raise Exception("error reading the PDF file")
+            raise Exception("error reading the PDF file" + str(e))
         
     elif file.name.endswith(".txt"):
         return file.read().decode("utf-8")
